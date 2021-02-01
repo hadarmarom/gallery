914c11135ae4d9b3b9f319e05b2ed4622dc8671a
hadarmarom <hadarmarom1@gmail.com>
2021-02-01 19:04:57 +0200

    projects and modals renderd. modal doesnt open

---
+++ js/main-controller.js
@@ -0,0 +1,55 @@
+'use strict'
+
+function init() {
+  renderPortabeloGrid()
+  // renderPortabeloModal()
+}
+
+function renderPortabeloGrid() {
+  var projs = getExProjects();
+  var strHtmls = projs.map(function (proj) {
+    return `<a class= " portfolio-link" data-toggle="modal" href="#portfolioModal${proj.id}">
+      <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+      </div>
+      <img class="img-fluid" src="img/portfolio/${proj.id}.jpg" alt="">
+      </a>
+      <div class="portfolio-caption">
+      <h4>${proj.labels[0]}</h4>
+      <p class="text-muted">${proj.labels[1]}</p>
+      </div>
+      </div>`
+  })
+  $('.portfolio-item').html(strHtmls)
+}
+
+function renderPortabeloModal() {
+  var projs = getExProjects();
+  var strHtmls = projs.map(function (proj) {
+
+    console.log('in');
+    return `     <h2>Project Name</h2>
+                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.jpg" alt="">
+                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+                  blanditiis
+                  dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae
+                  cupiditate,
+                  maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                <ul class="list-inline">
+                  <li>Date: January 2017</li>
+                  <li>Client:${proj.labels[0]}</li>
+                  <li>Category:${proj.labels[1]}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                  <i class="fa fa-times"></i>
+                  Close Project</button>`
+  })
+  document.querySelector('.modal-body').innerHTML = strHtmls;
+}
+
+function openMpdal() {
+  $('.portfolio-modal').hidden=false;
+}
---
+++ js/main-service.js
@@ -0,0 +1,47 @@
+var gExProjects = [
+    {
+        id: 'pacman',
+        name: 'Pacman',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/pacman',
+        publishedAt: 1448693940000,
+        labels: ['Pacman', 'Pacnam Game'],
+    }, {
+        id: 'touchTheNumbers',
+        name: 'touch The Numbers',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/touchTheNumbers',
+        publishedAt: 1448693940000,
+        labels: ['Touch The Numbers', 'Touch The Numbers'],
+    }, {
+        id: 'ballBoard',
+        name: 'Ball board',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/ballBoard',
+        publishedAt: 1448693940000,
+        labels: ['Ball Board', 'The Ball Board Game'],
+    }, {
+        id: 'todos',
+        name: 'Todos',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/todos',
+        publishedAt: 1448693940000,
+        labels: ['Todos', 'Todos Web'],
+    }, {
+        id: 'bookShop',
+        name: 'Book shop',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/bookShop',
+        publishedAt: 1448693940000,
+        labels: ['Book Shop', 'The Book Shop Web'],
+    }];
+function getExProjects() {
+    return gExProjects;
+}
+
+
